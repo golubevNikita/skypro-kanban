8d9cb37ec6bd15d676db250150a71a0e55ea9405
golubevNikita <vulivercushe.premedu@gmail.com>
2025-03-01 19:56:50 +0300

    react-hw1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+
 import "./App.css";
-import PopUp from "./components/PopUp";
-import Header from "./components/Header";
-import CardsItem from "./components/CardsItem";
+import PopUser from "./components/PopUps/PopUser";
+import PopNewCard from "./components/PopUps/PopNewCard";
+import PopBrowse from "./components/PopUps/PopBrowse";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -12,68 +15,12 @@ function App() {
   return (
     <>
       <div className="wrapper">
-        <PopUp />
+        <PopUser />
+        <PopNewCard />
+        <PopBrowse />
 
         <Header />
-
-        <main className="main">
-          <div className="container">
-            <div className="main__block">
-              <div className="main__content">
-                <div className="main__column column">
-                  <div className="column__title">
-                    <p>Без статуса</p>
-                  </div>
-                  <div className="cards">
-                    <CardsItem color="_orange" />
-                    <CardsItem color="_green" />
-                    <CardsItem color="_orange" />
-                    <CardsItem color="_purple" />
-                    <CardsItem color="_orange" />
-                  </div>
-                </div>
-
-                <div className="main__column">
-                  <div className="column__title">
-                    <p>Нужно сделать</p>
-                  </div>
-                  <div className="cards">
-                    <CardsItem color="_green" />
-                  </div>
-                </div>
-
-                <div className="main__column">
-                  <div className="column__title">
-                    <p>В работе</p>
-                  </div>
-                  <div className="cards">
-                    <CardsItem color="_green" />
-                    <CardsItem color="_purple" />
-                    <CardsItem color="_orange" />
-                  </div>
-                </div>
-
-                <div className="main__column">
-                  <div className="column__title">
-                    <p>Тестирование</p>
-                  </div>
-                  <div className="cards">
-                    <CardsItem color="_green" />
-                  </div>
-                </div>
-
-                <div className="main__column">
-                  <div className="column__title">
-                    <p>Готово</p>
-                  </div>
-                  <div className="cards">
-                    <CardsItem color="_green" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
+        <Main />
       </div>
 
       <script src="js/script.js"></script>
